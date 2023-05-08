4de0810425f1a911a47533d758b91f79cd5d9db4
YR <debora1628@khu.ac.kr>
2023-05-08 21:08:30 +0900

    reg

--- src/mainpage/Mainpage.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import { useNavigate, NavLink } from "react-router-dom";
-import React from "react";
-
-function Mainpage() {
-
-};
-
-export default Mainpage;
